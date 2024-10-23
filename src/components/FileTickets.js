4b4c54a410492b708ac20a256e83d548e3fb268f
henryR <henry11snake@hotmail.com>
2024-10-23 07:50:59 -0500

    correcciones varias de textos

--- src/components/FileTickets.js
+++ src/components/FileTickets.js
@@ -122,7 +122,7 @@ const FileTickets = ({
       //   deleteImageOld(); //Se elimina la imagen anterior del Storage si existiera
       // }
 
-      toast.success('Imagen subida con éxito!');
+      toast.success('Archivo subido con éxito!');
       setIsUpload(true);
       setShowModalFile({ ...showModal, active: false });
     }
@@ -405,7 +405,7 @@ const FileTickets = ({
                     d="M15 13.5H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
                   />
                 </svg>
-                Quitar Imágen
+                Quitar Archivo
               </button>
             )}
           </span>
